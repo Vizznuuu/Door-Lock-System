84199f2f62e8b80cabf3929547068ed3ee06abd2
Vishnu Prasad G <159981942+Vizznuuu@users.noreply.github.com>
2025-03-13 20:39:15 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,16 +1,15 @@
-// src/firebaseConfig.js
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAIAgKPDJWDbS_dCupYK0KTkYZJO-D9DkA",
-    authDomain: "doorlock-3661f.firebaseapp.com",
-    projectId: "doorlock-3661f",
-    storageBucket: "doorlock-3661f.firebasestorage.app",
-    messagingSenderId: "1038773553164",
-    appId: "1:1038773553164:web:d86d7f8c57ca4bf6df8629",
-    measurementId: "G-7DGGY03Q01"
-  };
+    apiKey: "AIzaSy*************YZJO-D9DkA",
+    authDomain: "doorlock-******.firebaseapp.com",
+    projectId: "doorlock-******",
+    storageBucket: "doorlock-*******.firebasestorage.app",
+    messagingSenderId: "************",
+    appId: "1:**************:web:d86d7f8c57ca4bf6df8629",
+    measurementId: "G-*********"
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
